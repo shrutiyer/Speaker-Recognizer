2c2b485a62814af56a539641fbd3ef1c8a85d68f
shrutiyer <shruti.iyer@students.olin.edu>
2017-04-29 01:00:20 -0400

    (I think) forward bakward works

--- speaker_recognizer/scripts/recognizer.py
+++ speaker_recognizer/scripts/recognizer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import rospy
-
 from python_speech_features import mfcc, delta, logfbank
 import scipy.io.wavfile as wav
 from scipy.cluster.vq import kmeans2, whiten, vq
@@ -31,7 +29,7 @@ class HMM(object):
         self.emissions = emissions # Matrix of emission probabilities. Size # of states by # of observation symbol
         self.states = states # Includes start (q0) and end (qF) states. Along with N hidden states
         self.state_len = len(self.states)
-        self.final_state_index = state_len-1
+        self.final_state_index = self.state_len-1
 
         # Gets set while training
         self.observations = None
@@ -44,7 +42,7 @@ class HMM(object):
         self.gamma = None
 
     def emission_prob(self, state, time): # TODO: observation arg?
-        return self.emissions[state][self.observations[time]]
+        return self.emissions[state][self.observations[time]-1]
 
     def get_observation_index(self, index):
         """Helper function to do indexing starting 1 instead"""
@@ -145,6 +143,7 @@ class HMM(object):
                     self.beta[state][time] += self.transitions[state][state_prime] *  \
                         self.emission_prob(state_prime,time+1) * \
                         self.beta[state_prime][time+1]
+                    print self.beta[state][time]
 
         # Termination
         for state in range(1, self.final_state_index):
@@ -176,10 +175,9 @@ class HMM(object):
         zeta_sum_num = 0
         zeta_sum_den = 0
         for time in range(self.get_observation_index(1), self.final_observation_index-1):
-            zeta_sum_num += self.zeta[state][state_prime]
+            zeta_sum_num += self.zeta[state][state_prime][time]
             for state_k in range(1, self.final_state_index):
-                zeta_sum_den += self.zeta[state][state_k]
-
+                zeta_sum_den += self.zeta[state][state_k][time]
         self.transitions[state][state_prime] = zeta_sum_num / zeta_sum_den
 
     def update_emissions(self, state, v_k):
@@ -201,17 +199,30 @@ class HMM(object):
             learn the transitions and emissions of the HMM."""
 
         # iterate until convergence
-        while True:
+        # TODO: Replace this with a while loop
+        for i in range(0,1):
             old_A = self.transitions
             old_B = self.emissions
             # expectation step
             self.forward()
+            print "ALPHA"
+            print self.alpha
+
             self.backward()
+            print "BETA"
+            print self.beta
+
             for time in range(self.get_observation_index(1), self.final_observation_index):
                 for state in range(1, self.final_state_index):
                     self.calc_gamma(state,time)
                     for state_prime in range(1, self.final_state_index):
                         self.calc_squiggle(state,state_prime,time)
+
+            print "GAMMA"
+            print self.gamma
+
+            print "ZETA"
+            print self.zeta
 
             # maximization step
             for state in range(1,self.final_state_index):
@@ -224,8 +235,8 @@ class HMM(object):
                     self.update_emissions(state,v_k)
             
             # TODO: how to determine convergence
-            print np.linalg.norm(old_A-self.transitions)
-            print np.linalg.norm(old_B-self.emissions)
+            # print np.linalg.norm(old_A-self.transitions)
+            # print np.linalg.norm(old_B-self.emissions)
                 
             # return A, B
             # return self.transitions, self.emissions
@@ -237,18 +248,21 @@ class HMM(object):
         """
         self.observations = observations
         self.observation_len = len(self.observations)
-        self.final_observation_index = observation_len-1
+        self.final_observation_index = self.observation_len-1
 
-        self.zeta = [[[0.0] * (self.observation_len) for i in range(self.state_len)] for j in range(self.state_len)]
-
+        self.zeta = np.zeros((self.state_len, self.state_len, self.observation_len))
+        self.gamma = np.zeros((self.state_len, self.observation_len))
+        for i in range(0,iterations):
+            # TODO: Do the Expectation Maximization step
+            self.baum_welch()
 
 if __name__ == '__main__':
     states = [0, 1, 2, 3] # Where 0 = start, 1 = hot, 2 = cold, 3 = final
     # Given ith state, how likely will it transition to jth state
-    transitions = np.array([0.0,0.5,0.5,0.0],[0.0,0.8,0.1,0.1],[0.0,0.1,0.8,0.1],[0.0,0.0,0.0,0.0])
+    transitions = np.array([[0.0,0.5,0.5,0.0],[0.0,0.8,0.1,0.1],[0.0,0.1,0.8,0.1],[0.0,0.0,0.0,0.0]])
     # Given ith state, probability of seeing 1, 2, 3 ice-creams.
-    emissions = np.array([0.0,0.0,0.0],[0.1,0.2,0.7],[0.7,0.2,0.1],[0.0,0.0,0.0])
-    observations = [1,3,2,2,1] # TODO: Change if necessary
+    emissions = np.array([[0.0,0.0,0.0],[0.1,0.2,0.7],[0.7,0.2,0.1],[0.0,0.0,0.0]])
+    observations = [2,3,3,2,3,2,3,2,2,3,1,3,3,1,1,1,2,1,1,1,3,1,2,1,1,1,2,3,3,2,3,2,2] # TODO: Change if necessary
     hmm = HMM(name='Brook', transitions=transitions, emissions=emissions, states=states)
 
-    hmm.train(observations)
+    hmm.train(observations,1)
