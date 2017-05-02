591fac123bb69665b0c327cf84c0788b3d0fa6bc
shrutiyer <shruti.iyer@students.olin.edu>
2017-05-02 15:57:51 -0400

    Clean up

--- speaker_recognizer/scripts/recognizer.py
+++ speaker_recognizer/scripts/recognizer.py
@@ -18,7 +18,6 @@ transition matrix: of size (n+2)x(n+2) where item at index (i,j) is the
 emissions matrix: of size mxn where item at index (i,j) is the probability 
                   of seeing ith observation in j state.
 """
-# TODO: Create V, a bunch of observation codebook. It contains v_k
 
 class HMM(object):
 
@@ -43,7 +42,7 @@ class HMM(object):
 
         self.gamma_sum = np.zeros(self.state_len)
 
-    def emission_prob(self, state, time): # TODO: observation arg?
+    def emission_prob(self, state, time):
         return self.emissions[state][self.observations[time]-1]
 
     def get_observation_index(self, index):
@@ -78,51 +77,6 @@ class HMM(object):
             self.alpha[self.final_state_index][self.final_observation_index] += self.alpha[state][self.final_observation_index]* \
                                                                     self.transitions[state][self.final_state_index]
 
-        # return self.alpha[self.final_state_index][self.final_observation_index]
-
-    def viterbi(self):
-        """ Given an observation sequence O and an HMM, lambda = (transitions, emissions), 
-        discover the best hidden state sequence Q """
-        path_probability = np.zeros((self.state_len, self.observation_len))
-
-        # Initialize viterbi and backpointer matrix
-        viterbi = np.zeros((self.state_len, self.observation_len))
-        backpointer = np.zeros((self.state_len,self.observation_len))
-
-        # Initialization
-        for state in range(1, final_state_index):
-            viterbi[state][1] = self.transitions[0][state]*self.emissions(state,self.get_observation(1))
-
-        # Recursion
-        for time in range(self.get_observation_index(2), self.observation_len): # Start from the second observation
-            for state in range(1, self.final_state_index): # Exclude start and end states here
-                max_viterbi = 0
-                max_viterbi_index = (-1, -1)
-                for state_prime in range(1, self.final_state_index):
-                    current_viterbi = viterbi[state_prime][time-1]*self.transitions[state_prime][state]* \
-                                        self.emission_prob(state,self.get_observation_index(time))
-                    if (current_viterbi > max_viterbi):
-                        max_viterbi = current_viterbi
-                        max_viterbi_index = (state_prime, time-1)
-
-                viterbi[state][time] = max_viterbi
-                # TODO: Tuples in a numpy array?
-                backpointer[state][time] = max_viterbi_index
-
-        # Termination
-        max_viterbi = 0
-        max_viterbi_index = (-1, -1)
-        for state in range(1, self.final_state_index):
-            current_viterbi = viterbi[state][self.final_observation_index]* \
-                                self.transitions[state][self.final_state_index]
-            if (current_viterbi > max_viterbi):
-                max_viterbi = current_viterbi
-                max_viterbi_index = (state, self.final_observation_index)
-        viterbi[self.final_state_index][final_observation_index] = max_viterbi
-        backpointer[self.final_state_index][final_observation_index] = max_viterbi_index
-
-        # TODO: Return Backtrace Path
-
     def backward(self):
         """ Given an HMM, lambda, determine the probability, beta, of seeing the 
             observations from time t+1 to the end, given that we are in state i at time t."""
@@ -135,11 +89,12 @@ class HMM(object):
             self.beta[state][self.final_observation_index] = self.transitions[state][self.final_state_index]
 
         # Recursion
-
-        # i: state
-        # j: state_prime
-        # t: time
-        # We want to calculate this numbers back to front
+        """
+        i: state
+        j: state_prime
+        t: time
+        We want to calculate this numbers back to front
+        """
         for time in range(self.final_observation_index-1,self.get_observation_index(1)-1,-1):
             for state in range(1, self.final_state_index): # Exclude start and end states here
                 for state_prime in range(1, self.final_state_index):
@@ -153,21 +108,22 @@ class HMM(object):
             update = self.transitions[0][state] * self.emission_prob(state,self.get_observation_index(1)) * self.beta[state][self.get_observation_index(1)]
             self.beta[0][self.get_observation_index(1)] += update
 
-        # # TODO: what does the backward algorithm return?
-        # return self.beta[0][0]
-
-    # zeta: the probability of being in state i and time t and state j and time t+1
-    # for updating self.transitions (A)
-    # i: state
-    # j: state_prime
+    """
+    zeta: the probability of being in state i and time t and state j and time t+1
+    for updating self.transitions (A)
+    i: state
+    j: state_prime
+    """
     def calc_squiggle(self, state, state_prime, time): 
         # Time should already be adjusted before calling the function
         self.zeta[state][state_prime][time] = (self.alpha[state][time] * self.transitions[state][state_prime] * \
                                                 self.emission_prob(state_prime,time+1) * self.beta[state_prime][time+1]) / \
                                                 self.alpha[self.final_state_index][self.final_observation_index]
 
-    # gamma: the probability of being in state j at time t
-    # for updating self.emissions (B)
+    """
+    gamma: the probability of being in state j at time t
+    for updating self.emissions (B)
+    """
     def calc_gamma(self, state, time):
         # Time should already be adjusted before calling the function
         self.gamma[state][time] = self.alpha[state][time] * self.beta[state][time] / self.alpha[self.final_state_index][self.final_observation_index]
@@ -210,57 +166,46 @@ class HMM(object):
     def baum_welch(self):
         """ Given an observation sequence O and the set of states in the HMM, 
             learn the transitions and emissions of the HMM."""
+        
+        # expectation step
+        self.forward()
+        # print "ALPHA"
+        # print self.alpha
 
-        # iterate until convergence
-        # TODO: Replace this with a while loop
-        for i in range(0,1):
-            old_A = self.transitions
-            old_B = self.emissions
-            # expectation step
-            self.forward()
-            # print "ALPHA"
-            # print self.alpha
+        self.backward()
+        # print "BETA"
+        # print self.beta
 
-            self.backward()
-            # print "BETA"
-            # print self.beta
+        for time in range(self.get_observation_index(1), self.observation_len):
+            for state in range(1, self.final_state_index):
+                self.calc_gamma(state,time)
+        
+        for time in range(self.get_observation_index(1), self.final_observation_index):
+            for state in range(1, self.final_state_index):
+                for state_prime in range(1, self.final_state_index):
+                    self.calc_squiggle(state,state_prime,time)
+        # print "GAMMA"
+        # print self.gamma
 
-            for time in range(self.get_observation_index(1), self.observation_len):
-                for state in range(1, self.final_state_index):
-                    self.calc_gamma(state,time)
-            
+        # print "ZETA"
+        # print self.zeta
+
+        # maximization step
+        for state in range(0,self.state_len-1):
+            for state_prime in range(0,self.state_len):
+                self.update_transitions(state,state_prime)
+
             for time in range(self.get_observation_index(1), self.final_observation_index):
-                for state in range(1, self.final_state_index):
-                    for state_prime in range(1, self.final_state_index):
-                        self.calc_squiggle(state,state_prime,time)
-            # print "GAMMA"
-            # print self.gamma
+                v_k = self.observations[time]
+                self.update_emissions(state,v_k)
+        
+        print "TRANSISTIONS"
+        print self.transitions
 
-            # print "ZETA"
-            # print self.zeta
-
-            # maximization step
-            for state in range(0,self.state_len-1):
-                for state_prime in range(0,self.state_len):
-                    self.update_transitions(state,state_prime)
-
-                for time in range(self.get_observation_index(1), self.final_observation_index):
-                    v_k = self.observations[time]
-                    self.update_emissions(state,v_k)
-            
-            print "TRANSISTIONS"
-            print self.transitions
-
-            print "EMISSIONS"
-            print self.emissions
-            
-            print "----------------------------------------------"
-            # TODO: how to determine convergence
-            # print np.linalg.norm(old_A-self.transitions)
-            # print np.linalg.norm(old_B-self.emissions)
-                
-            # return A, B
-            # return self.transitions, self.emissions
+        print "EMISSIONS"
+        print self.emissions
+        
+        print "----------------------------------------------"
             
     def train(self, observations, iterations=10):
         """
@@ -274,7 +219,6 @@ class HMM(object):
         self.zeta = np.zeros((self.state_len, self.state_len, self.observation_len))
         self.gamma = np.zeros((self.state_len, self.observation_len))
         for i in range(0,iterations):
-            # TODO: Do the Expectation Maximization step
             self.baum_welch()
 
 if __name__ == '__main__':
